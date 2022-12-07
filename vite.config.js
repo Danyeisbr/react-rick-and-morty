401d7cb9ce418672f3c4bcff10b4f2ff7d63478a
Dan <104391093+DanyiBR@users.noreply.github.com>
2022-12-06 23:49:24 -0500

    config updated

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://DanyiBR.github.io/react-rick-and-morty/",
+  base: "https://Danyeisbr.github.io/react-rick-and-morty/",
   plugins: [react()]
 })
